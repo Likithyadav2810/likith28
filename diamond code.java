0fc2ef64227600294395272a05febcb97255ebe5
Likithyadav2810 <kumarlikith73319@gmail.com>
2025-07-24 11:08:02 +0530

    hello

--- diamond code.java
+++ diamond code.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
        
-        int n=5;
+        int n=4;
         for (int i = 1; i <= n; i++) {
             for (int j = i; j < n; j++) {
                 System.out.print(" ");
